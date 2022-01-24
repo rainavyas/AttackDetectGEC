9263682ebdef2e3ce5032eddb69a26561a29a3fe
Vyas Raina <sayvaniar@gmail.com>
2022-01-24 18:02:20 +0000

    debug

--- confidence.py
+++ confidence.py
@@ -15,7 +15,7 @@ def negative_confidence(sentence, HappyModel, gen_args, device=torch.device('cpu
     '''
     sf = nn.Softmax(dim=0)
     # HappyModel.model.to(device)
-    model = HappyModel.model.to(device)
+    model = HappyModel.model
     tokenizer = HappyModel.tokenizer
     output_sentence = correct(HappyModel, sentence, gen_args)
     print(output_sentence)
